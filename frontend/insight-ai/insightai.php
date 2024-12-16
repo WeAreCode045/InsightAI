4c36f0f8103f4347b13649d39510ef82018cb6c0
Code045 <maurice@code045.nl>
2024-12-16 01:40:29 +0100

    Update insightai.php

--- frontend/insight-ai/insightai.php
+++ frontend/insight-ai/insightai.php
@@ -1,1 +1,11 @@
+<?php
 
+function insightai_shortcode() {
+    $file_path = plugin_dir_path(__FILE__) . '../../pdfpintar/public/index.php';
+    if (file_exists($file_path)) {
+        return file_get_contents($file_path);
+    } else {
+        return 'File not found.';
+    }
+}
+add_shortcode('insightai', 'insightai_shortcode');
