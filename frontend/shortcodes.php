0fac28e3f4ff2b6312c1de56f7203344bcd7b8bf
Code045 <maurice@code045.nl>
2024-12-16 05:37:36 +0100

    Update shortcodes.php

--- frontend/shortcodes.php
+++ frontend/shortcodes.php
@@ -1,31 +1,25 @@
+
 <?php
 
 add_shortcode('group_documents_dashboard', 'group_documents_dashboard_shortcode');
 
 function group_documents_dashboard_shortcode() {
-	ob_start();
-	include_once(INSIGHTAI . '/documents-dashboard.php');
-	return ob_get_clean();
+    ob_start();
+    include_once(INSIGHTAI . '/documents-dashboard.php');
+    return ob_get_clean();
 }
 
 add_shortcode('group_documents', 'group_documents_shortcode');
 
 function group_documents_shortcode() {
-	ob_start();
-	include_once(INSIGHTAI . '/pdfchat.php');
-	return ob_get_clean();
+    ob_start();
+    include_once(INSIGHTAI . '/pdfchat.php');
+    return ob_get_clean();
 }
 
 function insightai_shortcode() {
-    $file_path = ABSPATH . '/pdfpintar/public/index.php';
-    if (file_exists($file_path)) {
-        return file_get_contents($file_path);
-    } else {
-        return 'File not found.';
-    }
+    ob_start();
+    include_once(ABSPATH . '//pdfpintar/public/index.php');
+    return ob_get_clean();
 }
 add_shortcode('insightai', 'insightai_shortcode');
-
-
-
-
