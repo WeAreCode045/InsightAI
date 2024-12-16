d0f3f2d9f278e6d34b79b72b51b8c3147639e9b7
Code045 <maurice@code045.nl>
2024-12-16 01:42:49 +0100

    Update shortcodes.php

--- frontend/shortcodes.php
+++ frontend/shortcodes.php
@@ -16,6 +16,15 @@ function group_documents_shortcode() {
 	return ob_get_clean();
 }
 
+function insightai_shortcode() {
+    $file_path = plugin_dir_path(__FILE__) . '../pdfpintar/public/index.php';
+    if (file_exists($file_path)) {
+        return file_get_contents($file_path);
+    } else {
+        return 'File not found.';
+    }
+}
+add_shortcode('insightai', 'insightai_shortcode');
 
 
 
